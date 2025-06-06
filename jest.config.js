93e5e3e5d84fa0196208de536f0243ac341766b6
Jaehun <qkdwogns98@gmail.com>
2025-06-06 13:58:16 +0900

    install @testing-library/react-native, eslint-plugin-testing-library

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+
+  /** @see https://callstack.github.io/react-native-testing-library/docs/start/quick-start#quick-start */
+  files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  extends: ['plugin:testing-library/react'],
 };
