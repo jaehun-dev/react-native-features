5287b59b7481d90ca03dc9038cc1f45ca9e20422
Jaehun <qkdwogns98@gmail.com>
2025-06-06 16:33:24 +0900

    ios 환경변수 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import {Text, View} from 'react-native';
+import Config from 'react-native-config';
 
 export default function App() {
+  console.log(Config.ENV);
+
   return (
     <View>
       <Text>Hello World</Text>
