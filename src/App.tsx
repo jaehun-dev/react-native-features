6dde09d3bb0964fe569b42f0428a2e575f08e7d6
Jaehun <qkdwogns98@gmail.com>
2025-06-23 20:59:49 +0900

    chore: add SafeAreaProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import DrawerNavigator from './navigator/drawer-navigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <DrawerNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <DrawerNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
